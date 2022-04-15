0d2f0275eaa7d01ebf5b956e3fc7a29f5165b52f
AshwinRames <90181750+AshwinRames@users.noreply.github.com>
2022-04-15 12:34:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-var everyday = new Date();
-var hrs = everyday.getHours();
-if(hrs<10){
-    hrs=`0${hrs}`;
+function updateClock()
+{
+    var now = new Date();
+    var hou = now.getHours(),
+        min = now.getMinutes(),
+        days = now.getDay();
+        
+        var ids=["hours","minutes","day"];
+        var week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        var values=[hou,min,week[days]];
+        for(var i=0;i<ids.length;i++)
+         document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
-var min = everyday.getMinutes();
-if(min<10){
-    min=`0${min}`;
+
+function initClock(){
+    updateClock();
+    window.setInterval("updateClock()",1);
 }
-var time = hrs + " : " + min;
-document.getElementById('firsttime').innerHTML = time;
-document.getElementById('secondtime').innerHTML = time;
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var daysofweek = days[new Date().getDay()];
-document.getElementById('day').innerHTML = daysofweek;
